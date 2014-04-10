f9cc1b62ae08f28af33f3d2f3b276a7ea8f69eec
Ibokan <ibokan@12-07.local>
2014-04-10 12:12:20 +0800

    Initial Commit

---
+++ FaceAndText/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FaceAndText
+//
+//  Created by Ibokan on 14-4-10.
+//  Copyright (c) 2014年 CainiaoLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FaceAndTextTests/FaceAndTextTests.m
@@ -0,0 +1,34 @@
+//
+//  FaceAndTextTests.m
+//  FaceAndTextTests
+//
+//  Created by Ibokan on 14-4-10.
+//  Copyright (c) 2014年 CainiaoLiu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FaceAndTextTests : XCTestCase
+
+@end
+
+@implementation FaceAndTextTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
